656824a53445ee54b456e9c6e45c8632c461d0df
CarbrielysBasanta <103866063+CarbrielysBasanta@users.noreply.github.com>
2022-05-25 11:15:37 -0400

    Se añadieron comentarios al archivo src/api.js

--- src/api.js
+++ src/api.js
@@ -6,7 +6,11 @@ const path = require('path');
  * @module api
  */
 
-
+/**
+ * Api's port
+ * @name Port
+ * @path {SET}
+ */
 app.set('port', process.env.PORT || 3000);
 
 /**
@@ -19,7 +23,7 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'helloworld.html'));})
 
 /**
- * Index Route
+ * User's Route
  * @name Users
  * @path {POST} /users
  * 
@@ -27,7 +31,7 @@ app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'helloworld.htm
 app.post('/users', (req, res) => res.send('Welcome'))
 
 /**
- * Index Route
+ * Product's Route
  * 
  * @name Products
  * @path {DELETE} /products
@@ -36,13 +40,20 @@ app.post('/users', (req, res) => res.send('Welcome'))
 app.delete('/products', (req, res) => res.send('Welcome'))
 
 /**
- * Index Route
+ * Changes' Route
  * 
  * @name Changes
  * @path {PUT} /changes
  * 
  */
 app.put('/changes', (req, res) => res.send('Welcome'))
+
+/**
+ * Port is listening on port 3000
+ * @name Listen
+ * @path {LISTEN}
+ * 
+ */
 
 app.listen(app.get('port'), () => {
     console.log('Server listen on port', app.get('port'))
